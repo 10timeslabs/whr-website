3db02486022b310f7930875cd6212b2b81f90f4f
Farah Nasreen <farahnasreen2000@gmail.com>
2025-01-10 17:47:45 +0530

    Whr-ai landing page

--- src/app/teli/page.tsx
+++ src/app/teli/page.tsx
@@ -1,9 +1,76 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
+import styles from "./navbar.style.module.css";
+import Link from "next/link";
+import Logo from "/public/logo/croppedlogo.png";
+import Image from "next/image";
+import WhrAIText from "/public/logo/Whr.ai.png";
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
 
-const Page = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div>page</div>
-  )
-}
+    <nav
+      className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${
+        isScrolled ? "bg-white shadow-md" : "bg-transparent"
+      }`}
+    >
+      <div className="container mx-auto px-4 py-4 flex items-center justify-between ">
+        <div className="flex-shrink-0 flex items-center space-x-2">
+          <Link
+            href="/"
+            className="text-xl font-bold text-primary flex items-center gap-2"
+          >
+            <Image width="42" height="50" src={Logo} alt="nav-logo" />
+            <Image src={WhrAIText} alt="nav-logo" className="h-6 w-auto" />
+          </Link>
+        </div>
 
-export default Page
+        <div className="hidden md:flex space-x-8 bg-white px-8 py-2 rounded-xl">
+          <Link href="/teli" className="text-black hover:text-primary text-lg">
+            UseCases
+          </Link>
+          <Link href="/gtm" className="text-black hover:text-primary text-lg">
+            Solutions
+          </Link>
+          <Link href="/geni" className="text-black hover:text-primary text-lg">
+            Product
+          </Link>
+          <Link href="/about" className="text-black hover:text-primary text-lg">
+            About
+          </Link>
+        </div>
+
+        <div className="flex space-x-4">
+          {/* <Link
+            href="/signup"
+            className="px-8 py-2 text-white bg-primary rounded hover:bg-violet-400"
+          >
+            Sign Up
+          </Link>
+          <Link
+            href="/demo"
+            className="px-8 py-2 text-primary border border-primary rounded hover:bg-violet-900 hover:text-white"
+          >
+            Demo
+          </Link> */}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
