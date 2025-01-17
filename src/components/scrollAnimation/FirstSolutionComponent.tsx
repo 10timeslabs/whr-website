fde2523f9fafdd6112c395f1731c6b33316dede0
Akash Jindal <20akash.jindal@gmail.com>
2025-01-17 17:33:45 +0530

    correcting the animation

--- src/components/scrollAnimation/FirstSolutionComponent.tsx
+++ src/components/scrollAnimation/FirstSolutionComponent.tsx
@@ -14,29 +14,7 @@ interface Props {
 
 const FirstSolutionComponent = ({ heading, subHeading, imageOne, imageTwo }: Props) => {
 
-  const controls = useAnimation();
-  useEffect(() => {
-    // Start with the initial animation
-    controls.start("visible").then(() => {
-      // After the first animation completes, trigger fade-out
-      controls.start("fadeOut");
-    });
-  }, []);
-  const variants = {
-    initial: { bottom: "-200px", opacity: 1 },
-    visible: {
-      right: 80,
-      bottom: 80,
-      opacity: 1,
-      transition: { duration: 0.7, ease: "easeOut", delay: 0.2 },
-    },
-    fadeOut: {
-      opacity: 0,
-      transition: { duration: 0.5 }, // Fade-out duration
-    },
-  };                                   
-
-                                        
+  const [fadeOut, setFadeOut] = useState(false);
   return (
     <div className="h-[580px] flex flex-col items-center relative w-full overflow-hidden">
       <motion.div
@@ -44,9 +22,9 @@ const FirstSolutionComponent = ({ heading, subHeading, imageOne, imageTwo }: Pro
         initial={{ opacity: 1 }} // Initially visible
         whileInView={{ opacity: 0 }} // Fade out as it scrolls out of view
         viewport={{ once: true, amount: "some", margin: "0px 0px 0px 0px" }} // Trigger when the section is 50% out of view
-        transition={{ duration: .5, delay: .5 }}
+        transition={{ duration: .5 }}
       >
-      <div className='text-sm font-medium border border-color rounded-xl py-1 px-7'>How are we helpful</div>
+        <div className='text-sm font-medium border border-color rounded-xl py-1 px-7'>How are we helpful</div>
         <div className="font-semibold text-[40px] w-[40%]">
           Geo Provides Unique Features To Unlock On ground Demand
         </div>
@@ -71,9 +49,15 @@ const FirstSolutionComponent = ({ heading, subHeading, imageOne, imageTwo }: Pro
           width: "600px",
           height: "450px",
         }}
-        initial="initial"
-        animate={controls}
-        variants={variants}
+        initial={{ bottom: "-200px" }}
+        animate={fadeOut ? { opacity: 0, bottom: 80, right: 80 } : { opacity: 1, bottom: 80, right: 80 }}
+        transition={{ duration: .7 }}
+        onAnimationComplete={() => {
+          // Trigger fade-out after the first animation completes
+          if (!fadeOut) {
+            setTimeout(() => setFadeOut(true), 500); // Delay for the fade-out after the animation finishes
+          }
+        }}
       >
         <Image
           src={imageOne}
