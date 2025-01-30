297e9f3d1c1b3ea73b1db559821e49b4a2d3d92b
Akash Jindal <20akash.jindal@gmail.com>
2025-01-30 13:05:29 +0530

    clarity section responsiveness

--- src/components/clarityAnimation/Clarity.tsx
+++ src/components/clarityAnimation/Clarity.tsx
@@ -20,10 +20,10 @@ const Clarity = () => {
     }, [isLogoInView]);
     return (
         <div className='w-full flex flex-col items-center justify-center h-[550px] relative'>
-            <Image src={TopWaveImage} alt='wave' className='-z-10 absolute top-[-20%]' width={1300}/>
-            <Image src={BottomWaveImage} alt='wave' className='-z-10 absolute bottom-0' style={{width : "100%"}}/>
-            <div className='font-medium text-[32px]'>Clarity in Chaos</div>
-            <div className='text-[var(--secondary-text-color)] text-[20px]'>Real-world can be chaotic; beauty emerges from chaos. </div>
+            <Image src={TopWaveImage} alt='wave' className='-z-10 absolute top-[-20%]' width={1300} />
+            <Image src={BottomWaveImage} alt='wave' className='-z-10 absolute bottom-0' style={{ width: "100%" }} />
+            <div className='font-medium text-[32px] max-[575px]:text-[28px]'>Clarity in Chaos</div>
+            <div className='text-[var(--secondary-text-color)] text-[20px] text-center max-[575px]:text-[16px]'>Real-world can be chaotic; beauty emerges from chaos. </div>
             <div className='flex items-center justify-center w-full mt-10 h-[280px]'>
                 <div className='w-[800px] relative flex items-center justify-center h-full'>
                     <motion.div
@@ -43,8 +43,8 @@ const Clarity = () => {
 
                     {/* Typewriter Texts */}
                     {showText && (
-                        <>
-                            <div className="absolute top-0 bg-[#F8FBEA] text-[#8B8D52] text-[24px] rounded-xl py-1 px-2">
+                        <div className='text-[24px] max-[820px]:text-[18px]'>
+                            <div className="absolute top-0 left-[30%] bg-[#F8FBEA] text-[#8B8D52] rounded-xl py-1 px-2 max-[820px]:left-[40%] max-[650px]:left-[20%]">
                                 <Typewriter words={["Don't Just Go, Know"]} loop={1} typeSpeed={50} />
                                 <span className="font-semibold">
                                     <Typewriter words={[" WHR!"]} loop={1} typeSpeed={50} />
@@ -52,27 +52,27 @@ const Clarity = () => {
                             </div>
 
 
-                            <div className="absolute top-[80px] left-0 bg-[#FFF7F5] text-[#AE7562] text-[24px] rounded-xl py-1 px-2">
+                            <div className="absolute top-[80px] left-0 bg-[#FFF7F5] text-[#AE7562] rounded-xl py-1 px-2 max-[820px]:left-[10%] max-[650px]:top-[55px]">
                                 <Typewriter words={["Where could you"]} loop={1} typeSpeed={50} />
                                 <span className="font-semibold">
                                     <Typewriter words={[" FOCUS?"]} loop={1} typeSpeed={50} />
                                 </span>
                             </div>
 
-                            <div className="absolute top-[120px] right-0 bg-[#FEF6FF] text-[#9263A0] text-[24px] rounded-xl py-1 px-2">
+                            <div className="absolute top-[120px] right-0 bg-[#FEF6FF] text-[#9263A0] rounded-xl py-1 px-2 max-[820px]:right-[10%] max-[650px]:top-[180px]">
                                 <Typewriter words={["Where should you"]} loop={1} typeSpeed={50} />
                                 <span className="font-semibold">
                                     <Typewriter words={[" GO?"]} loop={1} typeSpeed={50} />
                                 </span>
                             </div>
 
-                            <div className="absolute top-[210px] bg-[#F4FAFF] text-[#486D95] text-[24px] rounded-xl py-1 px-2">
+                            <div className="absolute top-[210px] left-[30%] bg-[#F4FAFF] text-[#486D95] rounded-xl py-1 px-2 max-[820px]:left-[40%] max-[650px]:top-[250px] max-[650px]:left-[20%]">
                                 <Typewriter words={["Where would you"]} loop={1} typeSpeed={50} />
                                 <span className="font-semibold">
                                     <Typewriter words={[" BE?"]} loop={1} typeSpeed={50} />
                                 </span>
                             </div>
-                        </>
+                        </div>
                     )}
                 </div>
             </div>
