9b16775d77fa207cbdce263826858de19b80a46e
Farah Nasreen <farahnasreen2000@gmail.com>
2025-02-03 13:26:14 +0530

    Banner responsiveness

--- src/app/geo/[namespace]/[type]/page.tsx
+++ src/app/geo/[namespace]/[type]/page.tsx
@@ -19,7 +19,7 @@ import CircleAnimation from "@/components/circleAnimation/CircleAnimation";
 import CircleContainer from "@/components/circleAnimation/CircleContainer";
 import ProductsCarousel from "@/components/ProductSection/ProductsCarousel";
 import { companyLogos } from "../../../../../data/companyLogoData";
-import GridImage from '/public/herosection_grid.png'
+import GridImage from "/public/herosection_grid.png";
 import HelpfulContainer from "@/components/ProductSection/HelpfulContainer";
 import BeniftsSection from "@/components/ProductSection/BeniftsSection";
 import Conclusion from "@/components/ProductSection/Conclusion";
@@ -53,73 +53,160 @@ const page = () => {
   const dataToDisplay: any = usecaseData || defaultData;
   console.log("Endpoint:__", endpoint, dataToDisplay);
   return (
-    <div className="relative w-full flex flex-col gap-12 " style={{ paddingTop: pathname.split("/")[2] === "product" ? 120 : 0 }} >
-      {pathname.split("/")[2] === "product" &&
+    <div
+      className="relative w-full flex flex-col gap-12 "
+      style={{ paddingTop: pathname.split("/")[2] === "product" ? 120 : 0 }}
+    >
+      {pathname.split("/")[2] === "product" && (
         <>
-          <Image src={GridImage} alt="grid" className="absolute top-0 -z-[10]" />
+          <Image
+            src={GridImage}
+            alt="grid"
+            className="absolute top-0 -z-[10]"
+          />
           <div className="h-[100px] w-full absolute top-0 bg-gradient-to-b from-[rgba(165,184,198,0.2)] to-[rgba(132,168,204,0)]"></div>
         </>
-      }
+      )}
       {pathname.split("/")[2] === "usecases" ? (
-        <div className="relative w-full">
-          {/* Background Image */}
-          <Image
-            src={dataToDisplay.image}
-            alt="Banner Image"
-            className="h-[450px] md:h-[650px] w-full object-cover"
-          />
+        // <div className="relative w-full">
+        //   {/* Background Image */}
+        //   <Image
+        //     src={dataToDisplay.image}
+        //     alt="Banner Image"
+        //     className="h-[450px] md:h-[650px] w-full object-cover"
+        //   />
 
-          {/* Blurry Background */}
-          <div
-            className="absolute left-0 top-0 h-[450px] md:h-[650px] w-full md:w-[700px] p-6 rounded-r-md"
-            style={{
-              opacity: "0.9",
-              background: `${dataToDisplay.blurColor}`,
-              filter: "blur(100px)",
-              backdropFilter: "blur(50px)",
-              maskImage:
-                "linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 30%)",
-            }}
-          />
+        //   {/* Blurry Background */}
+        //   <div
+        //     className="absolute left-0 top-0 h-[450px] md:h-[650px] w-full md:w-[700px] p-6 rounded-r-md"
+        //     style={{
+        //       opacity: "0.9",
+        //       background: `${dataToDisplay.blurColor}`,
+        //       filter: "blur(100px)",
+        //       backdropFilter: "blur(50px)",
+        //       maskImage:
+        //         "linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 30%)",
+        //     }}
+        //   />
 
-          {/* Text Content */}
-          <div className="absolute left-5 md:left-[100px] top-10 h-auto max-w-[90%] md:max-w-[40%] p-4 md:p-6 rounded-r-md flex flex-col">
-            {/* Main Text */}
-            <div className="text-black font-bold text-[28px] md:text-[40px] mt-20 md:mt-32 leading-[34px] md:leading-[46px] break-words">
-              {dataToDisplay.text}
+        //   {/* Text Content */}
+        //   <div className="absolute left-5 md:left-[100px] top-10 h-auto max-w-[90%] md:max-w-[40%] p-4 md:p-6 rounded-r-md flex flex-col">
+        //     {/* Main Text */}
+        //     <div className="text-black font-bold text-[28px] md:text-[40px] mt-20 md:mt-32 leading-[34px] md:leading-[46px] break-words">
+        //       {dataToDisplay.text}
+        //     </div>
+
+        //     {/* Subtext */}
+        //     <div className="text-[#171717] text-sm md:text-lg leading-5 md:leading-6 mt-4 text-wrap break-words">
+        //       <p>{dataToDisplay.subtext}</p>
+        //     </div>
+        //   </div>
+        // </div>
+
+        <div className="w-full">
+          {/* For Screens ≤ 898px - Text Above, Image Below */}
+          <div className="max-[898px]:flex flex-col hidden mt-20 relative">
+            {/* Container with Gradient */}
+            <div
+              className="w-full p-6 rounded-r-md relative"
+              style={{
+                opacity: "0.9",
+                background: `${dataToDisplay.blurColor}`,
+                backdropFilter: "blur(50px)",
+              }}
+            >
+              {/* Text Content */}
+              <div className="w-[60%]">
+                <div className="text-black font-bold text-[28px] leading-[34px] z-30">
+                  {dataToDisplay.text}
+                </div>
+                <div className="text-black text-sm leading-5 mt-4 z-300">
+                  <p>{dataToDisplay.subtext}</p>
+                </div>
+              </div>
             </div>
 
-            {/* Subtext */}
-            <div className="text-[#171717] text-sm md:text-lg leading-5 md:leading-6 mt-4 text-wrap break-words">
-              <p>{dataToDisplay.subtext}</p>
+            {/* Image Below Text on Small Screens */}
+            <div className="w-full">
+              <Image
+                src={dataToDisplay.image}
+                alt="Banner Image"
+                className="w-full max-h-[650px] object-cover"
+              />
+            </div>
+          </div>
+
+          <div className="min-[899px]:block hidden w-full">
+            {/* Background Image */}
+            <Image
+              src={dataToDisplay.image}
+              alt="Banner Image"
+              className="h-[450px] md:h-[650px] w-full object-cover"
+            />
+
+            {/* Blurry Background */}
+            <div
+              className="absolute left-0 top-0 h-[450px] md:h-[650px] w-full md:w-[700px] p-6 rounded-r-md"
+              style={{
+                opacity: "0.9",
+                background: `${dataToDisplay.blurColor}`,
+                filter: "blur(100px)",
+                backdropFilter: "blur(50px)",
+                maskImage:
+                  "linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 30%)",
+              }}
+            />
+
+            {/* Text Content */}
+            <div className="absolute left-5 md:left-[100px] top-10 h-auto max-w-[90%] md:max-w-[40%] p-4 md:p-6 rounded-r-md flex flex-col">
+              {/* Main Text */}
+              <div className="text-black font-bold text-[28px] md:text-[40px] mt-20 md:mt-32 leading-[34px] md:leading-[46px] break-words">
+                {dataToDisplay.text}
+              </div>
+
+              {/* Subtext */}
+              <div className="text-[#171717] text-sm md:text-lg leading-5 md:leading-6 mt-4 text-wrap break-words">
+                <p>{dataToDisplay.subtext}</p>
+              </div>
             </div>
           </div>
         </div>
       ) : (
-        pathname.split("/")[2] === "solutions" &&
-        <div className="w-full flex items-center justify-center mt-[140px]">
-          <Section />
+        pathname.split("/")[2] === "solutions" && (
+          <div className="w-full flex items-center justify-center mt-[140px]">
+            <Section />
+          </div>
+        )
+      )}
+      {pathname.split("/")[2] !== "solutions" && (
+        <div className="flex flex-col gap-10 items-center mt-12 ">
+          <div className="text-sm font-medium border border-color rounded-xl py-1 px-7">
+            CUSTOMERS USING WHR
+          </div>
+          <AutoScroll icons={companyLogos} size="small" />
         </div>
       )}
-      {pathname.split("/")[2] !== "solutions" &&
-        <div className="flex flex-col gap-10 items-center mt-12 ">
-          <div className="text-sm font-medium border border-color rounded-xl py-1 px-7">CUSTOMERS USING WHR</div>
-          <AutoScroll icons={companyLogos} size="small" />
-        </div>}
       <div className="w-full flex justify-center">
         <ProblemSolution />
       </div>
-      {pathname.split("/")[2] === "product" &&
-        <div className="w-full flex items-center justify-center mt-10"><HelpfulContainer /></div>
-      }
-      {pathname.split("/")[2] === "product" &&
-        <div className="w-full flex items-center justify-center mt-10"><BeniftsSection /></div>
-      }
-      {pathname.split("/")[2] === "product" &&
-        <div className="w-full flex items-center justify-center">
-          <Conclusion heading="Make Search Smart" subheading="Go beyond basic lists and power your event strategy with deeper intel." />
+      {pathname.split("/")[2] === "product" && (
+        <div className="w-full flex items-center justify-center mt-10">
+          <HelpfulContainer />
         </div>
-      }
+      )}
+      {pathname.split("/")[2] === "product" && (
+        <div className="w-full flex items-center justify-center mt-10">
+          <BeniftsSection />
+        </div>
+      )}
+      {pathname.split("/")[2] === "product" && (
+        <div className="w-full flex items-center justify-center">
+          <Conclusion
+            heading="Make Search Smart"
+            subheading="Go beyond basic lists and power your event strategy with deeper intel."
+          />
+        </div>
+      )}
       {pathname.split("/")[2] === "solutions" ? (
         <div className="">
           {/* <Scroll /> */}
@@ -128,19 +215,24 @@ const page = () => {
           </div>
         </div>
       ) : (
-        pathname.split("/")[2] === "usecases" && <div className="w-full flex items-center justify-center">
-          {/* <LandingComponent /> */}
-          <Scroll />
+        pathname.split("/")[2] === "usecases" && (
+          <div className="w-full flex items-center justify-center">
+            {/* <LandingComponent /> */}
+            <Scroll />
+          </div>
+        )
+      )}
+      {(pathname.split("/")[2] === "solutions" ||
+        pathname.split("/")[2] === "usecases") && (
+        <div className="mb-5">
+          <VerticalScroll
+            mainHeading={
+              pathname.split("/")[2] === "usecases" ? "SOLUTIONS" : "USE CASES"
+            }
+            dataToDisplay={dataToDisplay}
+          />
         </div>
       )}
-      {(pathname.split("/")[2] === "solutions" || pathname.split("/")[2] === "usecases") && <div className="mb-5">
-        <VerticalScroll
-          mainHeading={
-            pathname.split("/")[2] === "usecases" ? "SOLUTIONS" : "USE CASES"
-          }
-          dataToDisplay={dataToDisplay}
-        />
-      </div>}
       <CircleContainer />
       <div>
         <ProductsCarousel />
--- src/app/gtm/[namespace]/[type]/page.tsx
+++ src/app/gtm/[namespace]/[type]/page.tsx
@@ -90,7 +90,75 @@ const page = () => {
         //   </div>
         //   <div className="mt-16"><AutoScroll icons={companyLogos} size="small"/></div>
         // </div>
-        <div className="relative w-full">
+      //   <div className="relative w-full">
+      //   {/* Background Image */}
+      //   <Image
+      //     src={dataToDisplay.image}
+      //     alt="Banner Image"
+      //     className="h-[450px] md:h-[650px] w-full object-cover"
+      //   />
+
+      //   {/* Blurry Background */}
+      //   <div
+      //     className="absolute left-0 top-0 h-[450px] md:h-[650px] w-full md:w-[700px] p-6 rounded-r-md"
+      //     style={{
+      //       opacity: "0.9",
+      //       background: `${dataToDisplay.blurColor}`,
+      //       filter: "blur(100px)",
+      //       backdropFilter: "blur(50px)",
+      //       maskImage:
+      //         "linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 30%)",
+      //     }}
+      //   />
+
+      //   {/* Text Content */}
+      //   <div className="absolute left-5 md:left-[100px] top-10 h-auto max-w-[90%] md:max-w-[40%] p-4 md:p-6 rounded-r-md flex flex-col">
+      //     {/* Main Text */}
+      //     <div className="text-black font-bold text-[28px] md:text-[40px] mt-20 md:mt-32 leading-[34px] md:leading-[46px] break-words">
+      //       {dataToDisplay.text}
+      //     </div>
+
+      //     {/* Subtext */}
+      //     <div className="text-[#171717] text-sm md:text-lg leading-5 md:leading-6 mt-4 text-wrap break-words">
+      //       <p>{dataToDisplay.subtext}</p>
+      //     </div>
+      //   </div>
+      // </div>
+
+      <div className="w-full">
+      {/* For Screens ≤ 898px - Text Above, Image Below */}
+      <div className="max-[898px]:flex flex-col hidden mt-20 relative">
+        {/* Container with Gradient */}
+        <div
+          className="w-full p-6 rounded-r-md relative"
+          style={{
+            opacity: "0.9",
+            background: `${dataToDisplay.blurColor}`,
+            backdropFilter: "blur(50px)",
+          }}
+        >
+          {/* Text Content */}
+          <div className="w-[60%]">
+            <div className="text-black font-bold text-[28px] leading-[34px] z-30">
+              {dataToDisplay.text}
+            </div>
+            <div className="text-black text-sm leading-5 mt-4 z-300">
+              <p>{dataToDisplay.subtext}</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Image Below Text on Small Screens */}
+        <div className="w-full">
+          <Image
+            src={dataToDisplay.image}
+            alt="Banner Image"
+            className="w-full max-h-[650px] object-cover"
+          />
+        </div>
+      </div>
+
+      <div className="min-[899px]:block hidden w-full">
         {/* Background Image */}
         <Image
           src={dataToDisplay.image}
@@ -124,6 +192,7 @@ const page = () => {
           </div>
         </div>
       </div>
+    </div>
       ) : (
         pathname.split("/")[2] === "solutions" &&
         <div className="w-full flex items-center justify-center mt-[140px]">
