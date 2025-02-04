2aca10092b550b387e2404ccba38e07af272e299
Akash Jindal <20akash.jindal@gmail.com>
2025-02-04 12:41:00 +0530

    added data to Benifits and highlights component for product

--- src/components/ProductSection/BeniftsSection.tsx
+++ src/components/ProductSection/BeniftsSection.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import SparkleImg from "/public/sparkle.png";
 import Image from 'next/image';
+import { geoProductJsonData } from "../../../data/geoProductData";
+import { gtmProductJsonData } from "../../../data/gtmProductData";
+import { usePathname } from "next/navigation";
 
 const BeniftsSection = () => {
-	const data = ["Draw polygons or pick entire cities.", "Boost ROI with relevant filters.", "Flexible viewing formats"]
+	const pathname = usePathname();
+	let dataSource = null;
+	if (pathname.includes("/geo/product")) {
+		dataSource = geoProductJsonData;
+	} else if (pathname.includes("/gtm/product")) {
+		dataSource = gtmProductJsonData;
+	}
+
+	const endpoint = pathname.split("/").pop()?.toLowerCase();
+	const productData = dataSource?.find((item: any) => item.id.toLowerCase() === endpoint);
+	const data = productData?.highlights
 	return (
 		<div className='w-[87%] flex justify-center items-start gap-5 max-[990px]:flex-col max-[990px]:items-center'>
 			<div className='flex flex-col min-w-[40%] gap-5 max-[990px]:items-center max-[600px]:gap-2'>
@@ -11,10 +24,10 @@ const BeniftsSection = () => {
 				<span className='text-[20px] text-[var(--secondary-text-color)] max-[990px]:text-center max-[600px]:text-[16px]'>A billion people go, do we know when, why, whr?</span>
 			</div>
 			<div className='w-[60%] flex flex-col gap-4 max-[990px]:w-full'>
-				{data.map((item, key) => (
+				{data?.map((item, key) => (
 					<div className={`w-full flex ${key === 1 ? "justify-end" : "justify-start"}`} key={key}>
 						<div className='w-[500px] bg-white rounded-2xl border border-[var(--border-color)] flex items-center gap-4 p-4'>
-							<Image src={SparkleImg} alt='sparkle' width={38} height={38}/>
+							<Image src={SparkleImg} alt='sparkle' width={38} height={38} />
 							<span className='font-medium text-xl'>{item}</span>
 						</div>
 					</div>
