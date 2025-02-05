c39e3dcca9a67bc16fe67f8120e94f3776f71691
Farah Nasreen <farahnasreen2000@gmail.com>
2025-02-05 18:12:18 +0530

    Uncomment Generatemetadata fn

--- src/app/gtm/[namespace]/[type]/page.tsx
+++ src/app/gtm/[namespace]/[type]/page.tsx
@@ -13,44 +13,44 @@ const Page = () => {
 
 export default Page;
 
-// export async function generateMetadata(context: any) {
+export async function generateMetadata(context: any) {
   
-//   const { namespace, type } = await context.params;
-//   let dataSource = null;
-//   if (namespace === "usecases") {
-//     dataSource = gtmUsecasesjsonData;
-//   } else if (namespace === "solutions") {
-//     dataSource = gtmSolutionsjsonData;
-//   }else if(namespace ==="product"){
-//     dataSource = gtmProductJsonData
-//   }
-//   const gtmData = dataSource?.find((item) => {
-//     return item.id.toLowerCase() === type;
-//   });
-//   const dataToDisplay: any = gtmData;
-//   console.log("id______________", namespace, type,dataToDisplay);
-//   if (!type || !namespace || !gtmData) {
-//     return {
-//       title: "404 - Not Found",
-//       description: "The page you are looking for does not exist.",
-//       openGraph: {
-//         title: "404 - Not Found",
-//         description: "The page you are looking for does not exist.",
-//         images: [], // You can include a default 404 image if desired
-//         // url: `${console_url}/detailpage?widgetId=${widgetId}&eventId=${eventId}`,
-//         type: "website",
-//       },
-//     };
-//   }
-//   return {
-//     title: dataToDisplay.metaData.title,
-//     description: dataToDisplay.metaData.description,
-//     openGraph: {
-//       title: dataToDisplay.metaData.title,
-//       description: dataToDisplay.metaData.description,
-//       images: dataToDisplay.metaData.image,
-//       // url: `${console_url}/detailpage?widgetId=${widgetId}&eventId=${eventId}`,
-//       type: "website",
-//     },
-//   };
-// }
+  const { namespace, type } = await context.params;
+  let dataSource = null;
+  if (namespace === "usecases") {
+    dataSource = gtmUsecasesjsonData;
+  } else if (namespace === "solutions") {
+    dataSource = gtmSolutionsjsonData;
+  }else if(namespace ==="product"){
+    dataSource = gtmProductJsonData
+  }
+  const gtmData = dataSource?.find((item) => {
+    return item.id.toLowerCase() === type;
+  });
+  const dataToDisplay: any = gtmData;
+  // console.log("id______________", namespace, type,dataToDisplay);
+  if (!type || !namespace || !gtmData) {
+    return {
+      title: "404 - Not Found",
+      description: "The page you are looking for does not exist.",
+      openGraph: {
+        title: "404 - Not Found",
+        description: "The page you are looking for does not exist.",
+        images: [], // You can include a default 404 image if desired
+        // url: `${console_url}/detailpage?widgetId=${widgetId}&eventId=${eventId}`,
+        type: "website",
+      },
+    };
+  }
+  return {
+    title: dataToDisplay.metaData.title,
+    description: dataToDisplay.metaData.description,
+    openGraph: {
+      title: dataToDisplay.metaData.title,
+      description: dataToDisplay.metaData.description,
+      images: dataToDisplay.metaData.image,
+      // url: `${console_url}/detailpage?widgetId=${widgetId}&eventId=${eventId}`,
+      type: "website",
+    },
+  };
+}
