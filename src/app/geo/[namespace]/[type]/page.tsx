85303e8084659a345f5e6e659d2c05ce7c0fa5a3
Farah Nasreen <farahnasreen2000@gmail.com>
2025-02-05 18:06:30 +0530

    Comment out Generatemetadata fn

--- src/app/geo/[namespace]/[type]/page.tsx
+++ src/app/geo/[namespace]/[type]/page.tsx
@@ -13,45 +13,45 @@ const Page = () => {
 
 export default Page;
 
-export async function generateMetadata(context: any) {
-    console.log("content",context)
-    const { namespace, type } = await context.params;
+// export async function generateMetadata(context: any) {
+//     console.log("content",context)
+//     const { namespace, type } = await context.params;
 
-  let dataSource = null;
-  if (namespace === "usecases") {
-    dataSource = geoUsecasesjsonData;
-  } else if (namespace === "solutions") {
-    dataSource = geoSolutionsjsonData;
-  } else if(namespace === "product"){
-    dataSource = geoProductJsonData;
-  }
-  const geoData = dataSource?.find((item) => {
-    return item.id.toLowerCase() === type;
-  });
+//   let dataSource = null;
+//   if (namespace === "usecases") {
+//     dataSource = geoUsecasesjsonData;
+//   } else if (namespace === "solutions") {
+//     dataSource = geoSolutionsjsonData;
+//   } else if(namespace === "product"){
+//     dataSource = geoProductJsonData;
+//   }
+//   const geoData = dataSource?.find((item) => {
+//     return item.id.toLowerCase() === type;
+//   });
 
-  const dataToDisplay: any = geoData;
-  if (!type || !namespace || !geoData) {
-    return {
-      title: "404 - Not Found",
-      description: "The page you are looking for does not exist.",
-      openGraph: {
-        title: "404 - Not Found",
-        description: "The page you are looking for does not exist.",
-        images: [], // You can include a default 404 image if desired
-        // url: `${console_url}/detailpage?widgetId=${widgetId}&eventId=${eventId}`,
-        type: "website",
-      },
-    };
-  }
-  return {
-    title: dataToDisplay.metaData.title,
-    description: dataToDisplay.metaData.description,
-    openGraph: {
-      title: dataToDisplay.metaData.title,
-      description: dataToDisplay.metaData.description,
-      images: dataToDisplay.metaData.image,
-      // url: `${console_url}/detailpage?widgetId=${widgetId}&eventId=${eventId}`,
-      type: "website",
-    },
-  };
-}
+//   const dataToDisplay: any = geoData;
+//   if (!type || !namespace || !geoData) {
+//     return {
+//       title: "404 - Not Found",
+//       description: "The page you are looking for does not exist.",
+//       openGraph: {
+//         title: "404 - Not Found",
+//         description: "The page you are looking for does not exist.",
+//         images: [], // You can include a default 404 image if desired
+//         // url: `${console_url}/detailpage?widgetId=${widgetId}&eventId=${eventId}`,
+//         type: "website",
+//       },
+//     };
+//   }
+//   return {
+//     title: dataToDisplay.metaData.title,
+//     description: dataToDisplay.metaData.description,
+//     openGraph: {
+//       title: dataToDisplay.metaData.title,
+//       description: dataToDisplay.metaData.description,
+//       images: dataToDisplay.metaData.image,
+//       // url: `${console_url}/detailpage?widgetId=${widgetId}&eventId=${eventId}`,
+//       type: "website",
+//     },
+//   };
+// }
