05f308d3faebde799b342ea5ec9c1eec675f81d3
Akash Jindal <20akash.jindal@gmail.com>
2025-02-11 15:35:07 +0530

    page card ui

--- src/components/PageCard.tsx
+++ src/components/PageCard.tsx
@@ -18,7 +18,7 @@ const PageCard = ({ image, heading, subheading, icon, href }: Props) => {
         <div> {React.cloneElement(icon, { width: 20, height: 20 })}</div>
         <div className='text-[24px] text-[#6750a4] font-medium'>{heading.substring(0,25)}{heading.length > 25 ? "..." :""}</div>
       </div>
-      <div>{subheading}</div>
+      <div className='text-[var(--secondary-text-color)]'>{subheading}</div>
       <div className='border border-[var(--primary-color)] rounded-md text-center px-5 py-2 w-fit text-[#6750a4]'>Learn More</div>
     </Link>
   )
