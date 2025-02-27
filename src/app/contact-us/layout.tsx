c07d362c7f08c588e345b30827a71162319a0f29
Farah Nasreen <farahnasreen2000@gmail.com>
2025-02-27 14:21:33 +0530

    Added og image

--- src/app/contact-us/layout.tsx
+++ src/app/contact-us/layout.tsx
@@ -2,11 +2,24 @@ import type { Metadata } from "next";
 import "../globals.css";
 
 export const metadata: Metadata = {
-  title: "Contact us - whr.ai",
-  description:
-    "Find out how WHR.AI uses cookies to enhance your experience. Read our Cookie Policy to understand data tracking, preferences, and control options.",
-};
-
+    title: "Contact us - whr.ai",
+    description: "Connect with us to learn how WHR.AI's Event Intelligence Platform can help you with real-time forecasting, demand planning, and maximizing event impact. Our team is here to assist you—reach out today!",
+   
+    openGraph: {
+      title: "Contact us - whr.ai",
+      description: "Connect with us to learn how WHR.AI's Event Intelligence Platform can help you with real-time forecasting, demand planning, and maximizing event impact. Our team is here to assist you—reach out today!",
+      images: [
+        {
+          url: "https://c1.10times.com/whr/home/static/image/contact_legal.png",
+          width: 1200,
+          height: 630,
+          alt: "GTM Event Intelligence Solutions",
+        },
+      ],
+      type: "website",
+    },
+  };
+  
 export default function RootLayout({
   children,
 }: {
---
+++ src/app/demo/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "Demo - whr.ai",
+  description: "Explore WHR.AI's demo page to experience our platform's features firsthand. Get insights into how our solutions work, understand key functionalities, and see how they can benefit you. Try it now and discover the possibilities!",
+ 
+  openGraph: {
+    title: "Demo - whr.ai",
+    description: "Explore WHR.AI's demo page to experience our platform's features firsthand. Get insights into how our solutions work, understand key functionalities, and see how they can benefit you. Try it now and discover the possibilities!",
+    images: [
+      {
+        url: "https://c1.10times.com/whr/home/static/image/contact_legal.png",
+        width: 1200,
+        height: 630,
+        alt: "GTM Event Intelligence Solutions",
+      },
+    ],
+    type: "website",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <div>{children}</div>
+    </>
+  );
+}
