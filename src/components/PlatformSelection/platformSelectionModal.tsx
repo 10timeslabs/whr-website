996d5c233d4b4a1ee079679048db802357d79923
Rajat <rajat@10times.com>
2025-06-24 15:52:56 +0530

    added payment links for geo & platform selection layer

--- src/components/PlatformSelection/platformSelectionModal.tsx
+++ src/components/PlatformSelection/platformSelectionModal.tsx
@@ -34,7 +34,7 @@ const PlatformSelectionModal = ({ isOpen, onClose, children }: Props) => {
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div
         ref={modalRef}
-        className="bg-white rounded-lg shadow-lg p-6 w-full max-w-lg relative"
+        className="bg-white rounded-lg shadow-lg p-6 w-full max-w-[90vw] lg:max-w-[60vw] relative"
       >
         <button
           className="text-lg absolute top-5 right-5 text-gray-500 hover:text-gray-700"
--- src/components/pricing/PriceCard.tsx
+++ src/components/pricing/PriceCard.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import TickIcon from "/public/priceTick.svg";
+import whrLogo from "/public/logo/croppedlogo.png";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import PlatformSelectionModal from "../PlatformSelection/platformSelectionModal";
@@ -47,59 +48,19 @@ const PriceCard = ({ data, isActive, isAnually }: Props) => {
   const queryString = new URLSearchParams(utm).toString();
 
   //   Test payment Links
-//   const paymentLinks: paymentLinks = {
-//     geo: {
-//       Essential: {
-//         monthly: `https://buy.stripe.com/test_cNi7sK8eU0cZekq4UU57W01?${queryString}`,
-//         yearly: `https://buy.stripe.com/test_eVqdR8eDif7T2BI9ba57W06?${queryString}`,
-//       },
-//       Scale: {
-//         monthly: `https://buy.stripe.com/test_bJedR852I2l7foudrq57W02?${queryString}`,
-//         yearly: `https://buy.stripe.com/test_00weVcdze3pbb8e3QQ57W05?${queryString}`,
-//       },
-//       Advance: {
-//         monthly: `https://buy.stripe.com/test_00w4gy1Qw1h36RYcnm57W03?${queryString}`,
-//         yearly: `https://buy.stripe.com/test_6oU6oGan2aRDfou5YY57W04?${queryString}`,
-//       },
-//       Enterprise: {
-//         monthly: `/demo?${queryString}`,
-//         yearly: `/demo?${queryString}`,
-//       },
-//     },
-//     gtm: {
-//       Essential: {
-//         monthly: `https://buy.stripe.com/test_cNi7sK8eU0cZekq4UU57W01?${queryString}`,
-//         yearly: `https://buy.stripe.com/test_eVqdR8eDif7T2BI9ba57W06?${queryString}`,
-//       },
-//       Scale: {
-//         monthly: `https://buy.stripe.com/test_bJedR852I2l7foudrq57W02?${queryString}`,
-//         yearly: `https://buy.stripe.com/test_00weVcdze3pbb8e3QQ57W05?${queryString}`,
-//       },
-//       Advance: {
-//         monthly: `https://buy.stripe.com/test_00w4gy1Qw1h36RYcnm57W03?${queryString}`,
-//         yearly: `https://buy.stripe.com/test_6oU6oGan2aRDfou5YY57W04?${queryString}`,
-//       },
-//       Enterprise: {
-//         monthly: `/demo?${queryString}`,
-//         yearly: `/demo?${queryString}`,
-//       },
-//     },
-//   };
-
-  //   Prod payment Links
   const paymentLinks: paymentLinks = {
     geo: {
       Essential: {
-        monthly: `https://buy.stripe.com/bJedR852I2l7foudrq57W02?${queryString}`,
-        yearly: `https://buy.stripe.com/bJe7sKcva9Nz2BIdrq57W07?${queryString}`,
+        monthly: `https://buy.stripe.com/test_bJe7sKcva9Nz2BIdrq57W07?${queryString}`,
+        yearly: `https://buy.stripe.com/test_14A8wO2UA0cZ3FMfzy57W0a?${queryString}`,
       },
       Scale: {
-        monthly: `https://buy.stripe.com/00w4gy1Qw1h36RYcnm57W03?${queryString}`,
-        yearly: `https://buy.stripe.com/eVqdR8eDif7T2BI9ba57W06?${queryString}`,
+        monthly: `https://buy.stripe.com/test_dRmfZg2UA0cZ1xEbji57W08?${queryString}`,
+        yearly: `https://buy.stripe.com/test_4gM28q66MgbXb8egDC57W0b?${queryString}`,
       },
       Advance: {
-        monthly: `https://buy.stripe.com/6oU6oGan2aRDfou5YY57W04?${queryString}`,
-        yearly: `https://buy.stripe.com/00weVcdze3pbb8e3QQ57W05?${queryString}`,
+        monthly: `https://buy.stripe.com/test_3cI6oGcva4tfgsyevu57W09?${queryString}`,
+        yearly: `https://buy.stripe.com/test_eVqbJ0br6bVHb8eafe57W0c?${queryString}`,
       },
       Enterprise: {
         monthly: `/demo?${queryString}`,
@@ -108,16 +69,16 @@ const PriceCard = ({ data, isActive, isAnually }: Props) => {
     },
     gtm: {
       Essential: {
-        monthly: `https://buy.stripe.com/bJedR852I2l7foudrq57W02?${queryString}`,
-        yearly: `https://buy.stripe.com/bJe7sKcva9Nz2BIdrq57W07?${queryString}`,
+        monthly: `https://buy.stripe.com/test_cNi7sK8eU0cZekq4UU57W01?${queryString}`,
+        yearly: `https://buy.stripe.com/test_eVqdR8eDif7T2BI9ba57W06?${queryString}`,
       },
       Scale: {
-        monthly: `https://buy.stripe.com/00w4gy1Qw1h36RYcnm57W03?${queryString}`,
-        yearly: `https://buy.stripe.com/eVqdR8eDif7T2BI9ba57W06?${queryString}`,
+        monthly: `https://buy.stripe.com/test_bJedR852I2l7foudrq57W02?${queryString}`,
+        yearly: `https://buy.stripe.com/test_00weVcdze3pbb8e3QQ57W05?${queryString}`,
       },
       Advance: {
-        monthly: `https://buy.stripe.com/6oU6oGan2aRDfou5YY57W04?${queryString}`,
-        yearly: `https://buy.stripe.com/00weVcdze3pbb8e3QQ57W05?${queryString}`,
+        monthly: `https://buy.stripe.com/test_00w4gy1Qw1h36RYcnm57W03?${queryString}`,
+        yearly: `https://buy.stripe.com/test_6oU6oGan2aRDfou5YY57W04?${queryString}`,
       },
       Enterprise: {
         monthly: `/demo?${queryString}`,
@@ -126,12 +87,52 @@ const PriceCard = ({ data, isActive, isAnually }: Props) => {
     },
   };
 
+  //   Prod payment Links
+  // const paymentLinks: paymentLinks = {
+  //   geo: {
+  //     Essential: {
+  //       monthly: `https://buy.stripe.com/dRmfZg2UA0cZ1xEbji57W08?${queryString}`,
+  //       yearly: `https://buy.stripe.com/4gM28q66MgbXb8egDC57W0b?${queryString}`,
+  //     },
+  //     Scale: {
+  //       monthly: `https://buy.stripe.com/3cI6oGcva4tfgsyevu57W09?${queryString}`,
+  //       yearly: `https://buy.stripe.com/eVqbJ0br6bVHb8eafe57W0c?${queryString}`,
+  //     },
+  //     Advance: {
+  //       monthly: `https://buy.stripe.com/14A8wO2UA0cZ3FMfzy57W0a?${queryString}`,
+  //       yearly: `https://buy.stripe.com/00w28qbr69Nzgsycnm57W0d?${queryString}`,
+  //     },
+  //     Enterprise: {
+  //       monthly: `/demo?${queryString}`,
+  //       yearly: `/demo?${queryString}`,
+  //     },
+  //   },
+  //   gtm: {
+  //     Essential: {
+  //       monthly: `https://buy.stripe.com/bJedR852I2l7foudrq57W02?${queryString}`,
+  //       yearly: `https://buy.stripe.com/bJe7sKcva9Nz2BIdrq57W07?${queryString}`,
+  //     },
+  //     Scale: {
+  //       monthly: `https://buy.stripe.com/00w4gy1Qw1h36RYcnm57W03?${queryString}`,
+  //       yearly: `https://buy.stripe.com/eVqdR8eDif7T2BI9ba57W06?${queryString}`,
+  //     },
+  //     Advance: {
+  //       monthly: `https://buy.stripe.com/6oU6oGan2aRDfou5YY57W04?${queryString}`,
+  //       yearly: `https://buy.stripe.com/00weVcdze3pbb8e3QQ57W05?${queryString}`,
+  //     },
+  //     Enterprise: {
+  //       monthly: `/demo?${queryString}`,
+  //       yearly: `/demo?${queryString}`,
+  //     },
+  //   },
+  // };
+
   const handlePlanClick = (planName: string, type: "monthly" | "yearly") => {
     if (gtmUser && !geoUser) {
       paymentLinks.gtm[planName][type];
-      window.open(paymentLinks["gtm"][planName][type], "_blank");
+      window.open(paymentLinks["gtm"][planName][type], "_self");
     } else if (!gtmUser && geoUser) {
-      window.open(paymentLinks["geo"][planName][type], "_blank");
+      window.open(paymentLinks["geo"][planName][type], "_self");
     } else {
       setModalOpen(true);
     }
@@ -174,8 +175,6 @@ const PriceCard = ({ data, isActive, isAnually }: Props) => {
           </div>
         </div>
         <Link
-          // href={paymentLinks[data.name][isAnually ? "yearly" : "monthly"]}
-          // target={data.name === "Enterprise" ? "_self" : "_blank"}
           className="bg-[var(--primary-color)] rounded-md text-white text-center py-2 drop-shadow-[2px_2px_5px_rgba(0,0,0,0.25)]"
           onClick={() =>
             handlePlanClick(data.name, isAnually ? "yearly" : "monthly")
@@ -189,32 +188,106 @@ const PriceCard = ({ data, isActive, isAnually }: Props) => {
         isOpen={isModalOpen}
         onClose={() => setModalOpen(false)}
       >
-        <h2 className="text-lg font-semibold mb-6">Select Platform</h2>
-        <div className="grid grid-cols-2 gap-6">
-          <Link
-            className="bg-white rounded-xl p-5 flex flex-col gap-3 drop-shadow-[2px_2px_5px_rgba(0,0,0,0.25)] col-span-1 h-32 text-center"
-            href={
-              gtmUser
-                ? paymentLinks["gtm"][data.name][
-                    isAnually ? "yearly" : "monthly"
-                  ]
-                : "https://geo.whr.ai/signup?redirect=whr-pricing"
-            }
-          >
-            <span className="font-semibold">GTM</span>
-          </Link>
-          <Link
-            className="bg-white rounded-xl p-5 flex flex-col gap-3 drop-shadow-[2px_2px_5px_rgba(0,0,0,0.25)] col-span-1 cursor-pointer text-center"
-            href={
-              geoUser
-                ? paymentLinks["geo"][data.name][
-                    isAnually ? "yearly" : "monthly"
-                  ]
-                : "https://geo.whr.ai/signup?redirect=whr-pricing"
-            }
-          >
-            <span className="font-semibold">GEO</span>
-          </Link>
+        {/* <h2 className="text-lg font-semibold">Select Platform</h2> */}
+        <div className="grid grid-cols-6 gap-6 mt-6">
+          <div className="bg-[#59478C] rounded-xl p-5 flex flex-col gap-3 drop-shadow-[2px_2px_5px_rgba(0,0,0,0.25)] col-span-6 lg:col-span-2 h-[20vh] lg:h-[50vh] justify-between">
+            <div className="flex flex-col gap-3">
+              <div className="text-base text-white font-semibold">
+                Save More
+                <div className="text-lg text-white font-bold">
+                  With Good Plans.
+                </div>
+              </div>
+              <div className="text-sm text-white">
+                Select your platform, sign up in minutes, and choose the perfect
+                plan to launch your journey.
+              </div>
+              <div className="text-white font-bold">{"->"}</div>
+            </div>
+            <div className="items-center gap-2 hidden lg:flex">
+              <div className="bg-white rounded-xl p-1 h-9 w-9 flex items-center justify-center shadow-lg">
+                <Image src={whrLogo} alt="tick" height={26} width={24} />
+              </div>
+              <span className="text-white font-bold text-lg">WHR.ai</span>
+            </div>
+          </div>
+          <div className="bg-white rounded-xl p-5 flex flex-col gap-3 drop-shadow-[2px_2px_5px_rgba(0,0,0,0.25)] col-span-3 lg:col-span-2 h-[50vh] justify-between">
+            <div className="flex flex-col gap-3">
+              <div className="flex items-center gap-2">
+                <div className="bg-white rounded-xl p-1 h-9 w-9 flex items-center justify-center shadow-lg">
+                  <Image src={whrLogo} alt="tick" height={26} width={24} />
+                </div>
+                <div className="font-bold text-2xl">GEO</div>
+              </div>
+              <div className="text-muted-foreground text-xl">
+                What You'll Get
+              </div>
+              <div className="flex gap-2 items-baseline text-sm">
+                <Image src={TickIcon} alt="tick" />
+                On-ground Event intelligence
+              </div>
+              <div className="flex gap-2 items-baseline text-sm">
+                <Image src={TickIcon} alt="tick" />
+                Location-based Insights
+              </div>
+              <div className="border border-b-[#A9A9AA] border-dashed"></div>
+            </div>
+
+            <Link
+              className="bg-[var(--primary-color)] rounded-md text-white text-center py-2 "
+              href={
+                geoUser
+                  ? paymentLinks["geo"][data.name][
+                      isAnually ? "yearly" : "monthly"
+                    ]
+                  : "https://geo.whr.ai/signup?redirect=whr-pricing"
+              }
+            >
+              Choose
+            </Link>
+          </div>
+
+          <div className="bg-white rounded-xl p-5 flex flex-col gap-3 drop-shadow-[2px_2px_5px_rgba(0,0,0,0.25)] col-span-3 lg:col-span-2 h-[50vh] justify-between">
+            <div className="flex flex-col gap-3">
+              <div className="flex items-center gap-2">
+                <div className="bg-white rounded-xl p-1 h-9 w-9 flex items-center justify-center shadow-lg">
+                  <Image src={whrLogo} alt="tick" height={26} width={24} />
+                </div>
+                <div className="font-bold text-2xl">GTM</div>
+              </div>
+              <div className="text-muted-foreground text-xl">
+                What You'll Get
+              </div>
+              <div className="overflow-y-auto h-[120px]">
+                <div className="flex gap-2 items-baseline text-sm">
+                  <Image src={TickIcon} alt="tick" />
+                  Go-to-Market Event Strategy
+                </div>
+                <div className="flex gap-2 items-baseline text-sm">
+                  <Image src={TickIcon} alt="tick" />
+                  Track your competitors
+                </div>
+                <div className="flex gap-2 items-baseline text-sm">
+                  <Image src={TickIcon} alt="tick" />
+                  Prospect events, people and companies
+                </div>
+              </div>
+              <div className="border border-b-[#A9A9AA] border-dashed"></div>
+            </div>
+
+            <Link
+              className="bg-[var(--primary-color)] rounded-md text-white text-center py-2 "
+              href={
+                gtmUser
+                  ? paymentLinks["gtm"][data.name][
+                      isAnually ? "yearly" : "monthly"
+                    ]
+                  : "https://gtm.whr.ai/signup?redirect=whr-pricing"
+              }
+            >
+              Choose
+            </Link>
+          </div>
         </div>
       </PlatformSelectionModal>
     </>
